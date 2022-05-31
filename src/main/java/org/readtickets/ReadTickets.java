98065f364a43dbc4f6b0d30a2338f6a703291331
Andrei Budevich <budevichac01@gmail.com>
2022-05-31 20:05:09 +0300

    create ReadTickets

---
+++ src/main/java/org/readtickets/ReadTickets.java
@@ -0,0 +1,26 @@
+package org.readtickets;
+
+import org.readtickets.model.Ticket;
+import org.readtickets.util.FileUtil;
+import org.readtickets.util.JsonUtil;
+
+import java.io.IOException;
+
+import static java.time.temporal.ChronoUnit.MINUTES;
+import static org.readtickets.util.DateTimeUtil.getAverageFlightTimeBetweenCities;
+import static org.readtickets.util.DateTimeUtil.getPercentile;
+
+public class ReadTickets {
+    public static void main(String[] args) throws IOException {
+
+        byte[] bytes = FileUtil.readFileAllBytes(args[0]);
+        Ticket[] tickets = JsonUtil.getTickets(bytes);
+
+        Double averageFlightTime = getAverageFlightTimeBetweenCities(tickets, MINUTES);
+
+        Double percentile = getPercentile(tickets, MINUTES, 90);
+
+        System.out.println(averageFlightTime);
+        System.out.println(percentile);
+    }
+}
